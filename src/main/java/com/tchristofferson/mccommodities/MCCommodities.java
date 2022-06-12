38fa44be4da66da2b59938ce50bc4ce0381e31d7
tchristofferson <longleadtrc@gmail.com>
2022-06-12 15:13:36 -0500

    Setup project

---
+++ src/main/java/com/tchristofferson/mccommodities/MCCommodities.java
@@ -0,0 +1,16 @@
+package com.tchristofferson.mccommodities;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MCCommodities extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
